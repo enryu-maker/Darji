64d540672551ed483b5741e836406e22ef6cc270
enryu-maker <akifkhan60067@gmail.com>
2021-12-29 12:41:11 +0530

    profile design

--- component/Home/Profile.js
+++ component/Home/Profile.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, TouchableOpacity, Image, ScrollView } from 'react-native'
+import { Text, StyleSheet, View, TouchableOpacity, Image, ScrollView, TextInput } from 'react-native'
 
 
 export default class Profile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = ({
+            Name:'',
+            Phone:'',
+            ShopName:'',
+            ShopAddr:'',
+            Email:'',
+            Website:'',
+        })
+    }
     render() {
         return (
             <View style={styles.Maincontainer}>
@@ -33,7 +44,76 @@ export default class Profile extends Component {
                     </TouchableOpacity>
                 </View>
                 <ScrollView style={styles.container}>
-
+                    <View style={styles.Format}>
+                    <Text style={styles.FormatText}>
+                        Name
+                    </Text>
+                    <TextInput
+                    style={styles.Input}
+                    placeholder='Name'
+                    placeholderTextColor={'rgb(37,36,39)'}
+                    onChangeText={(Name)=>{this.setState({Name})}}/>
+                    </View>
+                    <View style={styles.Format}>
+                    <Text style={styles.FormatText}>
+                        Phone
+                    </Text>
+                    <TextInput
+                    style={styles.Input}
+                    placeholder='Phone'
+                    onChangeText={(Phone)=>{this.setState({Phone})}}
+                    />
+                    </View>
+                    <View style={styles.Format}>
+                    <Text style={styles.FormatText}>
+                        Shop{'\n'}Name
+                    </Text>
+                    <TextInput
+                    style={styles.Input}
+                    placeholder='Shop Name'
+                    onChangeText={(ShopName)=>{this.setState({ShopName})}}
+                    />
+                    </View>
+                    <View style={[styles.Format]}>
+                    <Text style={styles.FormatText}>
+                    Shop{'\n'}Address 
+                    </Text>
+                    <TextInput
+                    style={[styles.Input,{marginRight:13}]}
+                    placeholder='Shop Address'
+                    onChangeText={(ShopAddr)=>{this.setState({ShopAddr})}}
+                    />
+                    </View>
+                    <View style={styles.Format}>
+                    <Text style={styles.FormatText}>
+                        Email 
+                    </Text>
+                    <TextInput
+                    style={styles.Input}
+                    placeholder='Email'
+                    onChangeText={(Email)=>{this.setState({Email})}}
+                    />
+                    </View>
+                    <View style={styles.Format}>
+                    <Text style={styles.FormatText}>
+                        Website 
+                    </Text>
+                    <TextInput
+                    style={styles.Input}
+                    placeholder='Website'
+                    onChangeText={(Website)=>{this.setState({Website})}}
+                    />
+                    </View>
+                    <View style={styles.Format}>
+                    <Text style={styles.FormatText}>
+                        Logo 
+                    </Text>
+                    <TextInput
+                    style={styles.Input}
+                    placeholder='Logo'
+                    onChangeText={(Name)=>{this.setState({Name})}}
+                    />
+                    </View>
                 </ScrollView>
                 
             </View>
@@ -75,5 +155,23 @@ const styles = StyleSheet.create({
         height:'100%',
         borderTopLeftRadius:20,
         borderTopRightRadius:20
+    },
+    Format:{
+        flexDirection:'row',
+        justifyContent:'space-around',
+        padding:7,
+        
+    },
+    FormatText:{
+        padding:15,
+        fontSize:18,
+        justifyContent:'center'
+    },
+    Input:{
+        borderBottomWidth:1,
+        width:'65%',
+        margin:5,
+        borderColor:'rgb(37,36,39)',
     }
+
 })
